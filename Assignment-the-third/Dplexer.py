822437269ecf2030649d31df48edf3a890395e65
Zachary Sisson <zsisson2@talapas-ln1.cm.cluster>
2021-08-10 11:57:06 -0700

    updated script

--- Assignment-the-third/Dplexer.py
+++ Assignment-the-third/Dplexer.py
@@ -17,20 +17,21 @@ def get_args():
 	parser.add_argument("-i1", "--insert1_file_name", help = "path to FASTQ for Read1 input file", type=str, required=True)
 	parser.add_argument("-i2", "--insert2_file_name", help = "path to FASTQ for Read1 input file", type=str, required=True)
 	parser.add_argument("-cutoff", "--cutoff", help = "index Q-score cutoff value for 'unkown/lowqual binning. Default = 30", type=int, required=False, default=30)
+	parser.add_argument("-o", "--output", help = "Pathway for output files", type=str, required=False, default='/projects/bgmp/zsisson2/bioinformatics/Bi622/Demultiplex/Assignment-the-third/Output_fastq_files/')
 	return parser.parse_args()
 
 args = get_args()
 
 #Define all functions that will be used in script
 def index_dictionary(path_to_tsv_file: str) -> dict:
-	'''function inputs a tsv file containing column information: sample, group, treatment, index, index-sequence and outputs a dictionary where the keys = unique index sequences and values = string of all other information (i.e sample, group, etc.). Note: the first line of the indexes file must be a header line, all subsequent lines sample information.'''
-	index_dictionary: dict[str,str] = {}
+	'''function inputs a tsv file containing column information: sample, group, treatment, index, index-sequence and outputs a dictionary where the keys = unique index sequences and values = a list containing the the R1 and R2 output files that will be generated from all other information (i.e sample, group, etc.). Note: the first line of the indexes file must be a header line, all subsequent lines sample information.'''
+	index_dictionary: dict[str,list] = {}
 	with open(path_to_tsv_file, "r") as indexes_file:
 		next(indexes_file)
 		for line in indexes_file.readlines():
 			line = line.strip('\n')
 			working_list = re.split("\t", line)
-			index_dictionary[working_list[4]] = str(str(working_list[0])+"_"+str(working_list[1])+"_"+str(working_list[2])+"_"+str(working_list[3]))
+			index_dictionary[working_list[4]] = [(str(working_list[0])+"_"+str(working_list[1])+"_"+str(working_list[2])+"_"+str(working_list[3])+"_L008_R1_001.fq.gz"),(str(working_list[0])+"_"+str(working_list[1])+"_"+str(working_list[2])+"_"+str(working_list[3])+"_L008_R2_001.fq.gz")]
 	return index_dictionary
 
 def reverse_complement(DNA_string: str) -> str:
@@ -62,36 +63,40 @@ def Qscore_eval(Qscore_line: str,cutoff: int):
 	else:
 		return False
 		
-def Read1opener(filename,L1,L2,L3,L4):
-	'''Function takes a file name and 4 lines, and writes those four lines to the Read 1 file'''	
-	file_name = str('/projects/bgmp/zsisson2/bioinformatics/Bi622/Demultiplex/Assignment-the-third/output_files/')+str(filename)+str('_L008__R1_001.fq.gz')
-	f = gzip.open(file_name,"at")
+def Read1opener(barcode,L1,L2,L3,L4):
+	'''Function takes an index sequence barcode and 4 lines, and writes those four lines to the Read 1 file associated with that index barcode from a created dictionary'''	
+	if barcode == "Unknown":
+		f = str(args.output+"Unknown_L008_R1_001.fastq")
+	elif barcode == "Swapped":
+		f = str(args.output+"Swapped_L008_R1_001.fastq")
+	else:
+		f = str(args.output+index_dictionary[barcode][0])
 	f.write(L1)
 	f.write(L2)
 	f.write(L3)
 	f.write(L4)
 	return
 
-def Read2opener(filename,L1,L2,L3,L4):
-	'''Function takes a file name and 4 lines, and writes those four lines to the Read 2 file'''	
-	file_name = str('/projects/bgmp/zsisson2/bioinformatics/Bi622/Demultiplex/Assignment-the-third/output_files/')+str(filename)+str('_L008__R2_001.fq.gz')
-	f = gzip.open(file_name,"at")
+def Read2opener(barcode,L1,L2,L3,L4):
+	'''Function takes an index sequence barcode and 4 lines, and writes those four lines to the Read 2 file associated with that index barcode from a created dictionary'''	
+	if barcode == "Unknown":
+		f = str(args.output+'Unknown_L008_R2_001.fastq')
+	elif barcode == "Swapped":
+		f = str(args.output+'Swapped_L008_R2_001.fastq')
+	else:
+		f = str(args.output+index_dictionary[barcode][1])
 	f.write(L1)
 	f.write(L2)
 	f.write(L3)
 	f.write(L4)
 	return
 
-def Read1closer(filename):
-	'''Function closes name of file'''	
-	file_name = str('/projects/bgmp/zsisson2/bioinformatics/Bi622/Demultiplex/Assignment-the-third/output_fastq_files/' +str(filename)+ '_L008__R1_001.fq.gz')
-	file_name.close()
-	return
-
-def Read2closer(filename):
-	'''Function closes name of file'''	
-	file_name = str('/projects/bgmp/zsisson2/bioinformatics/Bi622/Demultiplex/Assignment-the-third/output_fastq_files/'+str(filename)+ '_L008__R2_001.fq.gz')
-	file_name.close()
+def Readcloser(key):
+	'''Function closes name of R1 file associated with given dictionary key'''	
+	f1 = index_dictionary[key][0]
+	f2 = index_dictionary[key][1]
+	f1.close()
+	f2.close()
 	return
 
 
@@ -111,10 +116,24 @@ for num in range(len(Index_list)):
 	for i in range(len(Index_list)):
 		index_pair_list.append(str(Index_list[i])+'-'+str(Index_list[num]))
 		
-#Initalize dictionary where the keys = unique index-pairs and the values = occurences, initialized to zero 
+#Initalize dictionary where the keys = unique index-pairs and the values = occurences, initialized to zero. This will be used to generate stats report at end.
 index_pair_dict = {}
 for i in index_pair_list:
 	index_pair_dict[i] = 0
+
+
+#set output file path
+path=args.output
+
+#Open all files and keep them open in proper path
+for key,value in index_dictionary.items():
+	value[0] = gzip.open(str(path+index_dictionary[key][0]),"at")
+	value[1] = gzip.open(str(path+index_dictionary[key][1]),"at")
+Unknown_R1 = gzip.open(str(path+'Unknown_L008_R1_001.fastq'), "at")
+Unknown_R2 = gzip.open(str(path+'Unknown_L008_R2_001.fastq'), "at")
+Swapped_R1 = gzip.open(str(path+'Swapped_L008_R1_001.fastq'), "at")
+Swapped_R2 = gzip.open(str(path+'Swapped_L008_R2_001.fastq'), "at")
+
 
 
 #Open four input FASTQ files, and grab record from each file
@@ -154,35 +173,36 @@ while R1line4:
 		Read2opener("Unknown",R2_header,R2line2,R2line3,R2line4)
 		unknown_counter += 1
 		total_counter += 1
-
+		
 	else:
 		#Check to see if Index 2 is a perfect reverse complement of index 1 and if the index is in the reference list, If so, bin the reads to the proper file.
 		rev_comp = reverse_complement(I2line2)
-		if I1line2 == rev_comp and I1line2 in index_dictionary.keys():
-			Read1opener(index_dictionary[I1line2],R1_header,R1line2,R1line3,R1line4)
-			Read2opener(index_dictionary[I1line2],R2_header,R2line2,R2line3,R2line4)
+		if I1line2.strip('\n') == rev_comp and I1line2.strip('\n') in index_dictionary.keys():
+			Read1opener(index_dictionary[I1line2.strip('\n')],R1_header,R1line2,R1line3,R1line4)
+			Read2opener(index_dictionary[I1line2.strip('\n')],R2_header,R2line2,R2line3,R2line4)
 			matched_counter += 1
 			total_counter += 1
 			#Increments specific index-pair counter
-			key_to_search = str(I1line2)+'-'+str(rev_comp)
+			key_to_search = str(I1line2.strip('\n'))+'-'+str(rev_comp)
 			index_pair_dict[key_to_search] += 1
 
-		
+	
 		#If the indexes are perfect complements but are not found in the reference list, bin to unknown.
-		if I1line2 == rev_comp and I1line2 not in index_dictionary.keys():
+		if I1line2.strip('\n') == rev_comp and I1line2.strip('\n') not in index_dictionary.keys():
 			Read1opener("Unknown",R1_header,R1line2,R1line3,R1line4)
 			Read2opener("Unknown",R2_header,R2line2,R2line3,R2line4)
 			unknown_counter += 1
 			total_counter += 1
 		
+		
 		#If the indexes are NOT perfect reverse complements, BUT they are both found in the reference list, assign to index-hopped file.
-		if  I1line2 != rev_comp and I1line2 in index_dictionary.keys() and rev_comp in index_dictionary.keys():
+		if  I1line2.strip('\n') != rev_comp and I1line2.strip('\n') in index_dictionary.keys() and rev_comp in index_dictionary.keys():
 			Read1opener("Swapped",R1_header,R1line2,R1line3,R1line4)
 			Read2opener("Swapped",R2_header,R2line2,R2line3,R2line4)
 			hopped_counter += 1
 			total_counter += 1
 			#Increments specific index-pair counter
-			key_to_search = str(I1line2)+'-'+str(rev_comp)
+			key_to_search = str(I1line2.strip('\n'))+'-'+str(rev_comp)
 			index_pair_dict[key_to_search] += 1
 		
 		#If none of the above then bin to unknown.
@@ -213,35 +233,40 @@ while R1line4:
 	R2line3 = Read2.readline()
 	R2line4 = Read2.readline()
 
+
 #Close all files
-# for key, value in index_dictionary.items():
-# 	Read1closer(index_dictionary[key])
-# 	Read2closer(index_dictionary[key])
+Unknown_R1.close() 
+Unknown_R2.close() 
+Swapped_R1.close()
+Swapped_R2.close()
+for key,value in index_dictionary.items():
+	Readcloser(key)
+	
 
 #sort index-paired dictionary numerically by value and calculate total read-pairs for matches (hopped and dual-matched)
 total_matched_hopped = hopped_counter+matched_counter
 sorted(index_pair_dict, key=lambda i: int(index_pair_dict[i]))
 
 #Generate report of file contents
-with open('/home/zsisson2/bgmp/bioinformatics/Bi622/Demultiplex/Assignment-the-third/output_files/stats.md',"w") as fto:
+with open('/home/zsisson2/bgmp/bioinformatics/Bi622/Demultiplex/Assignment-the-third/stats.md',"w") as fto:
 	fto.write(str('Statistic' + '\t' + 'Value' + '\t' + 'Percentage of Whole'+'\n'))
-	fto.write(str('Total Number of Read-Pairs' + '\t' + str(total_counter) + '\t' + str((total_counter/total_counter)*100) +'%'+'\n'))
-	fto.write(str('Number of Unknown Read-Pairs' + '\t' + str(unknown_counter) + '\t' + str((unknown_counter/total_counter)*100) +'%'+'\n'))
-	fto.write(str('Number of Index-Hopped Read-Pairs' + '\t' + str(hopped_counter) + '\t' + str((hopped_counter/total_counter)*100) +'%'+'\n'))
-	fto.write(str('Number of Dual-Matched Read-Pairs' + '\t' + str(matched_counter) + '\t' + str((matched_counter/total_counter)*100) +'%'+'\n'))
+	fto.write(str('Total_Number_of_Read-Pairs' + '\t' + str(total_counter) + '\t' + str(round(total_counter/total_counter*100,2)) +'%'+'\n'))
+	fto.write(str('Dual-Matched_Read-Pairs' + '\t' + str(matched_counter) + '\t' + str(round(matched_counter/total_counter*100,2)) +'%'+'\n'))
+	fto.write(str('Index-Hopped_Read-Pairs' + '\t' + str(hopped_counter) + '\t' + str(round(hopped_counter/total_counter*100,2)) +'%'+'\n'))
+	fto.write(str('Unknown_Read-Pairs' + '\t' + str(unknown_counter) + '\t' + str(round(unknown_counter/total_counter*100,2)) +'%'+'\n'))
 	fto.write(str('\n'))
-	fto.write(str('Breakdown of Matched Pairs:'))
+	fto.write(str('Breakdown_of_Matched Pairs:'))
 	fto.write(str('\n'))
-	fto.write(str('Index Pair'+'\t'+'Number of Read-Pairs'+'\t'+'Percentage of Total Matched/Hopped Pair'+ 'Percentage of Total'))
+	fto.write(str('Index_Pair'+'\t'+'#_of_Read-Pairs'+'\t'+'%_of_Matched/Hopped'+ '\t' + '%_of_Total_Matched'+'\n'))
 	for key, value in index_pair_dict.items():
 		fto.write(key)
 		fto.write('\t')
 		fto.write(str(value))
 		fto.write('\t')
-		fto.write(str((value/total_matched_hopped)*100))
+		fto.write(str(round(value/total_matched_hopped*100,2)))
 		fto.write('%')
 		fto.write('\t')
-		fto.write(str((value/total_counter)*100))
+		fto.write(str(round(value/total_counter*100,2)))
 		fto.write('%')
 		fto.write('\n')
 
