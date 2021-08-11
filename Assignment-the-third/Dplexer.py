3ecbefdd3c6d1a5ec6096866b33cb863c56e646f
Zachary Sisson <zsisson2@talapas-ln1.cm.cluster>
2021-08-11 11:08:15 -0700

    updated script

--- Assignment-the-third/Dplexer.py
+++ Assignment-the-third/Dplexer.py
@@ -22,16 +22,22 @@ def get_args():
 
 args = get_args()
 
+
+#set output file path
+path=args.output
+
 #Define all functions that will be used in script
 def index_dictionary(path_to_tsv_file: str) -> dict:
-	'''function inputs a tsv file containing column information: sample, group, treatment, index, index-sequence and outputs a dictionary where the keys = unique index sequences and values = a list containing the the R1 and R2 output files that will be generated from all other information (i.e sample, group, etc.). Note: the first line of the indexes file must be a header line, all subsequent lines sample information.'''
+	'''function inputs a tsv file containing column information: sample, group, treatment, index, index-sequence and outputs a dictionary where the keys = unique index sequences and values = a list containing the the R1 and R2 output files that will be generated from all other information (i.e sample, group, etc.). Note: the first line of the indexes file must be a header line, all subsequent lines sample information. Function also hard codes output files for Unknown and swapped reads.'''
 	index_dictionary: dict[str,list] = {}
 	with open(path_to_tsv_file, "r") as indexes_file:
 		next(indexes_file)
 		for line in indexes_file.readlines():
 			line = line.strip('\n')
 			working_list = re.split("\t", line)
-			index_dictionary[working_list[4]] = [(str(working_list[0])+"_"+str(working_list[1])+"_"+str(working_list[2])+"_"+str(working_list[3])+"_L008_R1_001.fq.gz"),(str(working_list[0])+"_"+str(working_list[1])+"_"+str(working_list[2])+"_"+str(working_list[3])+"_L008_R2_001.fq.gz")]
+			index_dictionary[working_list[4]] = [(str(working_list[0])+"_"+str(working_list[1])+"_"+str(working_list[2])+"_"+str(working_list[3])+"_L008_R1_001.fq"),(str(working_list[0])+"_"+str(working_list[1])+"_"+str(working_list[2])+"_"+str(working_list[3])+"_L008_R2_001.fq")]
+	index_dictionary['Unknown'] = ['Unknown_L008_R1_001.fq','Unknown_L008_R2_001.fq']
+	index_dictionary['Swapped'] = ['Swapped_L008_R1_001.fq','Swapped_L008_R2_001.fq']
 	return index_dictionary
 
 def reverse_complement(DNA_string: str) -> str:
@@ -65,30 +71,18 @@ def Qscore_eval(Qscore_line: str,cutoff: int):
 		
 def Read1opener(barcode,L1,L2,L3,L4):
 	'''Function takes an index sequence barcode and 4 lines, and writes those four lines to the Read 1 file associated with that index barcode from a created dictionary'''	
-	if barcode == "Unknown":
-		f = str(args.output+"Unknown_L008_R1_001.fastq")
-	elif barcode == "Swapped":
-		f = str(args.output+"Swapped_L008_R1_001.fastq")
-	else:
-		f = str(args.output+index_dictionary[barcode][0])
-	f.write(L1)
-	f.write(L2)
-	f.write(L3)
-	f.write(L4)
+	index_dictionary[barcode][0].write(L1)
+	index_dictionary[barcode][0].write(L2)
+	index_dictionary[barcode][0].write(L3)
+	index_dictionary[barcode][0].write(L4)
 	return
 
 def Read2opener(barcode,L1,L2,L3,L4):
 	'''Function takes an index sequence barcode and 4 lines, and writes those four lines to the Read 2 file associated with that index barcode from a created dictionary'''	
-	if barcode == "Unknown":
-		f = str(args.output+'Unknown_L008_R2_001.fastq')
-	elif barcode == "Swapped":
-		f = str(args.output+'Swapped_L008_R2_001.fastq')
-	else:
-		f = str(args.output+index_dictionary[barcode][1])
-	f.write(L1)
-	f.write(L2)
-	f.write(L3)
-	f.write(L4)
+	index_dictionary[barcode][1].write(L1)
+	index_dictionary[barcode][1].write(L2)
+	index_dictionary[barcode][1].write(L3)
+	index_dictionary[barcode][1].write(L4)
 	return
 
 def Readcloser(key):
@@ -122,18 +116,10 @@ for i in index_pair_list:
 	index_pair_dict[i] = 0
 
 
-#set output file path
-path=args.output
-
 #Open all files and keep them open in proper path
 for key,value in index_dictionary.items():
-	value[0] = gzip.open(str(path+index_dictionary[key][0]),"at")
-	value[1] = gzip.open(str(path+index_dictionary[key][1]),"at")
-Unknown_R1 = gzip.open(str(path+'Unknown_L008_R1_001.fastq'), "at")
-Unknown_R2 = gzip.open(str(path+'Unknown_L008_R2_001.fastq'), "at")
-Swapped_R1 = gzip.open(str(path+'Swapped_L008_R1_001.fastq'), "at")
-Swapped_R2 = gzip.open(str(path+'Swapped_L008_R2_001.fastq'), "at")
-
+	value[0] = open(str(path+index_dictionary[key][0]),"at")
+	value[1] = open(str(path+index_dictionary[key][1]),"at")
 
 
 #Open four input FASTQ files, and grab record from each file
@@ -163,54 +149,54 @@ R2line3 = Read2.readline()
 R2line4 = Read2.readline()
 
 #Perform this loop so long as there is a record present in memory:
-while R1line4:
+while R1line1:
 	#append the index1 and index2 sequence to the header lines for the reads sequences
 	R1_header = str(R1line1.strip('\n') + ' ' + I1line2.strip('\n')+ '-' + I2line2)
 	R2_header = str(R2line1.strip('\n') + ' ' + I1line2.strip('\n')+ '-' + I2line2)
 	#Determine avg quality score of index lines. If either of the indexes in the pair contain an unknown base call (N) or an avg Q-score below the cutoff then bin to unknown. 
 	if Qscore_eval(I1line4,30) == False or Qscore_eval(I2line4,30) == False:
-		Read1opener("Unknown",R1_header,R1line2,R1line3,R1line4)
-		Read2opener("Unknown",R2_header,R2line2,R2line3,R2line4)
+		Read1opener('Unknown',R1_header,R1line2,R1line3,R1line4)
+		Read2opener('Unknown',R2_header,R2line2,R2line3,R2line4)
 		unknown_counter += 1
 		total_counter += 1
 		
-	else:
-		#Check to see if Index 2 is a perfect reverse complement of index 1 and if the index is in the reference list, If so, bin the reads to the proper file.
-		rev_comp = reverse_complement(I2line2)
-		if I1line2.strip('\n') == rev_comp and I1line2.strip('\n') in index_dictionary.keys():
-			Read1opener(index_dictionary[I1line2.strip('\n')],R1_header,R1line2,R1line3,R1line4)
-			Read2opener(index_dictionary[I1line2.strip('\n')],R2_header,R2line2,R2line3,R2line4)
-			matched_counter += 1
-			total_counter += 1
-			#Increments specific index-pair counter
-			key_to_search = str(I1line2.strip('\n'))+'-'+str(rev_comp)
-			index_pair_dict[key_to_search] += 1
-
 	
-		#If the indexes are perfect complements but are not found in the reference list, bin to unknown.
-		if I1line2.strip('\n') == rev_comp and I1line2.strip('\n') not in index_dictionary.keys():
-			Read1opener("Unknown",R1_header,R1line2,R1line3,R1line4)
-			Read2opener("Unknown",R2_header,R2line2,R2line3,R2line4)
-			unknown_counter += 1
-			total_counter += 1
-		
-		
-		#If the indexes are NOT perfect reverse complements, BUT they are both found in the reference list, assign to index-hopped file.
-		if  I1line2.strip('\n') != rev_comp and I1line2.strip('\n') in index_dictionary.keys() and rev_comp in index_dictionary.keys():
-			Read1opener("Swapped",R1_header,R1line2,R1line3,R1line4)
-			Read2opener("Swapped",R2_header,R2line2,R2line3,R2line4)
-			hopped_counter += 1
-			total_counter += 1
-			#Increments specific index-pair counter
-			key_to_search = str(I1line2.strip('\n'))+'-'+str(rev_comp)
-			index_pair_dict[key_to_search] += 1
-		
-		#If none of the above then bin to unknown.
-		else:
-			Read1opener("Unknown",R1_header,R1line2,R1line3,R1line4)
-			Read2opener("Unknown",R2_header,R2line2,R2line3,R2line4)
-			unknown_counter += 1
-			total_counter += 1
+	#Check to see if Index 2 is a perfect reverse complement of index 1 and if the index is in the reference list, If so, bin the reads to the proper file.
+	rev_comp = reverse_complement(I2line2)
+	elif I1line2.strip('\n') == rev_comp and I1line2.strip('\n') in index_dictionary.keys():
+		Read1opener(I1line2.strip('\n'),R1_header,R1line2,R1line3,R1line4)
+		Read2opener(I1line2.strip('\n'),R2_header,R2line2,R2line3,R2line4)
+		matched_counter += 1
+		total_counter += 1
+		#Increments specific index-pair counter
+		key_to_search = str(I1line2.strip('\n'))+'-'+str(rev_comp)
+		index_pair_dict[key_to_search] += 1
+
+
+	#If the indexes are perfect complements but are not found in the reference list, bin to unknown.
+	elif I1line2.strip('\n') == rev_comp and I1line2.strip('\n') not in index_dictionary.keys():
+		Read1opener('Unknown',R1_header,R1line2,R1line3,R1line4)
+		Read2opener('Unknown',R2_header,R2line2,R2line3,R2line4)
+		unknown_counter += 1
+		total_counter += 1
+	
+	
+	#If the indexes are NOT perfect reverse complements, BUT they are both found in the reference list, assign to index-hopped file.
+	elif  I1line2.strip('\n') != rev_comp and I1line2.strip('\n') in index_dictionary.keys() and rev_comp in index_dictionary.keys():
+		Read1opener('Swapped',R1_header,R1line2,R1line3,R1line4)
+		Read2opener('Swapped',R2_header,R2line2,R2line3,R2line4)
+		hopped_counter += 1
+		total_counter += 1
+		#Increments specific index-pair counter
+		key_to_search = str(I1line2.strip('\n'))+'-'+str(rev_comp)
+		index_pair_dict[key_to_search] += 1
+	
+	#If none of the above then bin to unknown.
+	else:
+		Read1opener('Unknown',R1_header,R1line2,R1line3,R1line4)
+		Read2opener('Unknown',R2_header,R2line2,R2line3,R2line4)
+		unknown_counter += 1
+		total_counter += 1
 
 #Grab next record for each file and continue
 	R1line1 = Read1.readline()
@@ -235,10 +221,6 @@ while R1line4:
 
 
 #Close all files
-Unknown_R1.close() 
-Unknown_R2.close() 
-Swapped_R1.close()
-Swapped_R2.close()
 for key,value in index_dictionary.items():
 	Readcloser(key)
 	
